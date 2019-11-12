3cbfa632c7ea029cd60708634f6aebb9f3776eba
mavin <chb221@lehigh.edu>
2019-11-12 14:25:30 -0500

    update

---
+++ icpc2018/North Central North America/Lipschitz.java
@@ -0,0 +1,44 @@
+package icpc;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class solution {
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in); 
+        int total = scan.nextInt();
+        
+        double retval = 0;
+        double[][] array = new double[total][2];
+        
+        for(int i = 0 ; i < total; i ++) {
+            array[i][0] = scan.nextDouble();
+            array[i][1] = scan.nextDouble();
+        }
+        
+        Arrays.sort(array,new java.util.Comparator<double[]>() {
+            public int compare(double[] a, double[] b) {
+                return Double.compare(a[0], b[0]);
+            }
+        });
+        
+        for(int j = 0 ; j < total - 1 ; j ++) {
+        	
+        	double x = array[j][0] - array[j+1][0];
+        	double y = array[j][1] - array[j+1][1];
+        	
+        	if(x * y < 0) {
+        		x = 0 - x;
+        	}
+        	
+        	if(retval < y/x) {
+        		retval = y/x;
+        	}
+        }
+        
+        System.out.print(retval);
+        
+    }
+}
---
+++ icpc2018/North Central North America/RationalRatio.java
@@ -0,0 +1,64 @@
+package icpc;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class solution {
+    
+    public static void main(String[] args) {
+        
+    	Scanner scan = new Scanner(System.in); 
+        String total = scan.next();
+        
+        int repeat = scan.nextInt();
+        int dot = total.indexOf('.');
+        long gcd = 0;
+        
+        String first = total.substring(0,dot);
+        String second = total.substring(dot+1, total.length());
+        String sub = total.substring(dot+1, total.length() - repeat);
+        String deno = "";
+        
+        for(int i = 0 ; i < repeat ; i++) {
+        	deno = deno + "9";
+        }
+        
+        for(int i = 0 ; i < sub.length(); i++) {
+        	deno = deno + "0";
+        }
+        
+        long number = Long.parseLong(first);
+        long numerator = Long.parseLong(second);
+        long denominator = Long.parseLong(deno);
+        
+        if(sub.length() != 0) {
+        	numerator = numerator - Long.parseLong(sub);
+        }
+        
+        if(numerator == 0) {
+        	denominator = 1;
+        } else {
+        	gcd = gcd(numerator, denominator);
+        	numerator = numerator / gcd;
+        	denominator = denominator / gcd;
+        }
+        
+        System.out.print((denominator * number + numerator) + "/" + denominator);
+        scan.close();
+    
+    }
+    
+    public static long gcd(long a, long b) {
+    	
+    	if(a < b) {
+    		if(b % a == 0) {
+    			return a;
+    		}
+    		return gcd(a ,b % a);
+    	}
+    	if(a % b == 0) {
+    		return b;
+    	}
+    	return gcd(a % b, b);
+    }
+}
