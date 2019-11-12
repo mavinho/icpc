1c941a26692b1039e625094ba7596b1b27d23671
mavinho <57306908+mavinho@users.noreply.github.com>
2019-11-12 14:36:43 -0500

    Update Palindromes.java

--- icpc2018/North Central North America/Palindromes.java
+++ icpc2018/North Central North America/Palindromes.java
@@ -1,8 +1,10 @@
-package icpc;
+/*
+https://open.kattis.com/problems/kaleidoscopicpalindromes
+*/
 
 import java.util.Scanner;
 
-public class solution {
+public class Solution {
 	
 	public static void main(String[] args) {
 		
@@ -10,10 +12,10 @@ public class solution {
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
-		int count = b-a+1;
+		int count = b - a + 1;
 		
-		for(int i = a ; i <= b ; i++) {
-			for(int j = 2 ; j <= c ; j++) {
+		for (int i = a; i <= b; i++) {
+			for (int j = 2; j <= c; j++) {
 				if(!isPalindrome(toBase(i,j))) {
 					count--;
 					break;
@@ -22,38 +24,30 @@ public class solution {
 		}
 		
 		System.out.print(count);
-		
 		scan.close();
 	}
 	
 	public static String toBase(int number, int base) {
-		
 		String str = "";
-		
 		while(number != 0) {
 			int temp = number % base;
 			number = number / base;
 			str = temp + str;
 		}
-		
 		return str;
 	}
 	
 	public static boolean isPalindrome(String str) {	
-		
 		return isPalindrome(str, 0, str.length()-1);
 	}
 	
 	public static boolean isPalindrome(String str, int start, int end) {
-		
 		if(start >= end) {
 			return true;
 		}
-		
 		if(str.charAt(start) != str.charAt(end)) {
 			return false;
 		}
-		
 		return isPalindrome(str, start + 1, end - 1);
 	}
 }
