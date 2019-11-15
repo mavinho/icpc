c617c8dbddf5b97f09dae15fc069237928de9db2
mavin <chb221@lehigh.edu>
2019-11-15 11:34:16 -0500

    new repository

---
+++ icpc2019/North East North America/addemup.java
@@ -0,0 +1,61 @@
+package coding;
+
+import java.util.Scanner;
+
+public class addemup {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[100000001];
+        int total = scan.nextInt();
+        int sum = scan.nextInt();
+        int temp = 0;
+        int contemp = 0;
+        boolean find = false;
+
+        for (int i = 0; i < total; i++) {
+            temp = scan.nextInt();
+            contemp = convert(temp);
+            if(array[sum-temp] != 0) {
+                find = true;
+                break;
+            } else if(array[sum-contemp] != 0) {
+                find = true;
+                break;
+            }
+            array[temp] = 1;
+            array[contemp] = 1;
+        }
+        
+        if (find)
+            System.out.print("YES");
+        else
+            System.out.print("NO");
+    }
+
+    public static int convert(int number) {
+        String str = "" + number;
+        String retval = "";
+        char ch;
+
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            if (ch == '1')
+                retval = '1' + retval;
+            else if (ch == '2')
+                retval = '2' + retval;
+            else if (ch == '5')
+                retval = '5' + retval;
+            else if (ch == '6')
+                retval = '9' + retval;
+            else if (ch == '8')
+                retval = '8' + retval;
+            else if (ch == '9')
+                retval = '6' + retval;
+            else
+                return 0;
+        }
+        return Integer.parseInt(retval);
+    }
+}
